75aaf0b61509588c0628bdddf5b52718a29379ae
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-06-11 23:08:04 -0300

    chore: CRUD of book class  changed to controller

--- src/app.js
+++ src/app.js
@@ -15,26 +15,4 @@ conexao.once('open', () => {
 const app = express();
 routes(app);
 
-app.get('/livros/:id', (req, res) => {
-  const index = buscaLivros(req.params.id);
-  res.status(200).json(livros[index]);
-});
-
-app.post('/livros', (req, res) => {
-  livros.push(req.body);
-  res.status(201).send('Livro cadastrado!');
-});
-
-app.put('/livros/:id', (req, res) => {
-  const index = buscaLivros(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.status(200).json(livros[index]);
-});
-
-app.delete('/livros/:id', (req, res) => {
-  const index = buscaLivros(req.params.id);
-  livros.splice(index, 1);
-  res.status(200).send('Livro removido com sucesso!');
-});
-
 export default app;
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,26 @@ import livro from '../models/Livro.js';
 
 export default class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao buscar livro` });
+    }
+  }
+
+  static async listarLivrosPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao buscar livros` });
+    }
   }
 
   static async cadastrarLivro(req, res) {
@@ -11,11 +29,35 @@ export default class LivroController {
       const novoLivro = await livro.create(req.body);
       res
         .status(201)
-        .json({ message: 'Livro criado com sucesso', livro: novoLivro });
+        .json({ message: 'Livro criado com sucesso!', livro: novoLivro });
     } catch (erro) {
       res
         .status(500)
         .json({ message: `${erro.message} - falha ao cadastrar livro` });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: 'Livro atualizado com sucesso!' });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao atualizar livro` });
+    }
+  }
+
+  static async excluirLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: 'Livro deletado com sucesso!' });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao deletar livro` });
     }
   }
 }
